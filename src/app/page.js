85b3af53f4f9dd65ef5c32aa00d9db209c7ad1a0
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-05-21 16:37:25 +0600

    Improved homepage UI and featured cars section

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import {
   useState,
 } from "react";
 
+import Link from "next/link";
+
 import Navbar from "@/components/Navbar";
 
 import Footer from "@/components/Footer";
@@ -39,17 +41,21 @@ const Home = () => {
 
       <div
         className="
-        h-[50vh]
+        bg-black
+        text-white
+        h-[70vh]
         flex
         justify-center
         items-center
         flex-col
+        text-center
+        px-5
       "
       >
 
         <h1
           className="
-          text-5xl
+          text-6xl
           font-bold
           mb-5
         "
@@ -59,10 +65,63 @@ const Home = () => {
 
         <p
           className="
-          text-xl
+          text-2xl
+          mb-8
         "
         >
-          Car Rental Platform
+          Premium Car Rental Platform
+        </p>
+
+        <Link href="/explore-cars">
+
+          <button
+            className="
+            bg-white
+            text-black
+            px-8
+            py-3
+            rounded-lg
+            font-semibold
+            hover:bg-gray-200
+            duration-300
+            cursor-pointer
+          "
+          >
+            Explore Cars
+          </button>
+
+        </Link>
+
+      </div>
+
+
+
+      {/* SECTION TITLE */}
+
+      <div
+        className="
+        text-center
+        py-16
+      "
+      >
+
+        <h2
+          className="
+          text-5xl
+          font-bold
+          mb-4
+        "
+        >
+          Featured Cars
+        </h2>
+
+        <p
+          className="
+          text-gray-600
+          text-lg
+        "
+        >
+          Choose your dream car
         </p>
 
       </div>
@@ -79,19 +138,23 @@ const Home = () => {
         grid-cols-1
         md:grid-cols-2
         lg:grid-cols-3
-        gap-6
-        mb-20
+        gap-8
+        pb-20
       "
       >
 
-        {cars.map(car => (
+        {cars.slice(0, 6).map(car => (
 
           <div
             key={car._id}
             className="
             border
-            p-5
-            rounded-xl
+            rounded-2xl
+            overflow-hidden
+            shadow-lg
+            hover:scale-105
+            duration-300
+            bg-white
           "
           >
 
@@ -99,43 +162,74 @@ const Home = () => {
               src={car.image}
               alt=""
               className="
-              h-[220px]
+              h-[250px]
               w-full
               object-cover
-              rounded-lg
-              mb-4
             "
             />
 
-            <h2
+            <div
               className="
-              text-2xl
-              font-bold
+              p-5
             "
             >
-              {car.carName}
-            </h2>
 
-            <p
-              className="
-              mt-2
-            "
-            >
-              Type:
-              {" "}
-              {car.carType}
-            </p>
+              <h2
+                className="
+                text-3xl
+                font-bold
+                mb-3
+              "
+              >
+                {car.carName}
+              </h2>
 
-            <p
-              className="
-              mt-2
-            "
-            >
-              Price:
-              {" "}
-              $
-              {car.dailyRentalPrice}
-            </p>
+              <p
+                className="
+                mb-2
+                text-lg
+              "
+              >
+                Type:
+                {" "}
+                {car.carType}
+              </p>
+
+              <p
+                className="
+                mb-5
+                text-lg
+              "
+              >
+                Price:
+                {" "}
+                $
+                {car.dailyRentalPrice}
+                /day
+              </p>
+
+              <Link
+                href={`/cars/${car._id}`}
+              >
+
+                <button
+                  className="
+                  bg-black
+                  text-white
+                  px-5
+                  py-2
+                  rounded-lg
+                  hover:bg-gray-800
+                  duration-300
+                  cursor-pointer
+                "
+                >
+                  View Details
+                </button>
+
+              </Link>
+
+            </div>
 
           </div>
         ))}
